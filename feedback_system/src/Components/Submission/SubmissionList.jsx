f42d7289d57a98440453945aa2fafbeaf7a334ab
sumansauravmay <sumansauravmay12345@gmail.com>
2024-05-14 18:08:42 +0530

    All submission feedback page

--- feedback_system/src/Components/Submission/SubmissionList.jsx
+++ feedback_system/src/Components/Submission/SubmissionList.jsx
@@ -1,16 +1,57 @@
 import React from "react";
 import "./Submission.css";
-import { Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Input, Stack, Text, Box, Button, Container } from "@chakra-ui/react";
+import { RepeatIcon } from '@chakra-ui/icons'
+import { Link } from "react-router-dom";
 
 const SubmissionList = () => {
   let arr = JSON.parse(localStorage.getItem("feedback")) || [];
 
-
+const handleRefresh=()=>{
+  window.location.reload();
+}
 
 
   return (
     <div>
-      <main>
+
+
+<Flex justifyContent={'space-evenly'} mt={50}>
+<Stack direction={'column'}>
+  <Heading>Aromatic Bar</Heading>
+  <Text>{arr.length} records founds</Text>
+</Stack>
+
+<Stack direction={'row'}>
+  <Box>
+  <Input 
+  border="1px solid gray"
+  htmlSize={4}
+   width={{lg:'200px', md:"auto", sm:"auto", base:"auto"}} 
+  placeholder="Search"/>
+  </Box>
+<Box border="1px solid gray" height={10} width={10} onClick={handleRefresh}>
+<RepeatIcon marginLeft={3} marginTop={2}/>
+</Box>
+
+<Box>
+  <Link to="/">
+
+<Button bg={"#48A44C"} color={'white'}
+_hover={{
+  background:"red"
+}}
+>Add New</Button>
+  </Link>
+</Box>
+
+ 
+</Stack>
+
+</Flex>
+
+
+      <main style={{marginTop:"100px"}}>
         <table>
           <thead>
             <tr>
@@ -23,10 +64,10 @@ const SubmissionList = () => {
               <th>Dining Experience</th>
             </tr>
           </thead>
-          {/* <tbody> */}
+        
             {arr.length>0?arr.map((feedback) => (
               <tbody>
-              <tr key={feedback.name}>
+              <tr key={feedback.email}>
                 <td>{feedback.name}</td>
                 <td>{feedback.email}</td>
                 <td>{feedback.phone}</td>
